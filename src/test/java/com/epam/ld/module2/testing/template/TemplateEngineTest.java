314bffeae46a31b9f500cf84199383ae8ffe0305
Sergey_Ryabtsev <Sergey_Ryabtsev@epam.com>
2021-10-14 22:27:11 +0600

    1 generate message

--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,4 +1,23 @@
 package com.epam.ld.module2.testing.template;
 
+import com.epam.ld.module2.testing.Client;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TemplateEngineTest {
+    TemplateEngine templateEngine;
+
+    @Test
+    public void generateMessageTest() {
+        //given
+        Template template = new Template();
+        Client client = new Client();
+        String expectedResult = "from: test@mail.ru, to: petrov@mail.ru body: hello my friend";
+
+        //when
+        String result = templateEngine.generateMessage(template, client);
+
+        //then
+        Assertions.assertEquals(result, expectedResult);
+    }
 }
